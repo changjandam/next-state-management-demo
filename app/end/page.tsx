1703167b0e6f52f0336492937f2f05ae61ad894c
Jimmy Hu <jimmyhu.coding@gmail.com>
2023-10-08 01:02:32 +0800

    add home page link to end page

--- app/end/page.tsx
+++ app/end/page.tsx
@@ -1,16 +1,17 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import { useRouter } from "next/navigation";
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 const EndPage = () => {
   const router = useRouter();
   return (
-    <div className="flex flex-col w-1/3">
-      <h1 className="text-4xl mb-4">End Page</h1>
-      <button className="w-fit" onClick={() => router.back()}>
+    <div className='flex flex-col w-1/3'>
+      <h1 className='text-4xl mb-4'>End Page</h1>
+      <button className='w-fit' onClick={() => router.back()}>
         Back to previous page
       </button>
+      <Link href='/'>Back to home page</Link>
     </div>
   );
 };
